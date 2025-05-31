d3e03a76878c7812a85520c7160abf2842fa6fe7
athayanajwan <athaya123hafidz@gmail.com>
2025-05-31 07:31:28 +0700

    Update TaskRepository.java

--- tasque/src/main/java/com/example/tasque/repository/TaskRepository.java
+++ tasque/src/main/java/com/example/tasque/repository/TaskRepository.java
@@ -3,18 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.example.tasque.repository;
-import com.example.tasque.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, String>{
-    @Query("SELECT COUNT(u) FROM User u")
-    long countUsers();
-    
-    Optional<User> findByUsername(String username);
-    
-    Optional<User> findByEmail(String email);
-    
-    Optional<User> findById(String id);
+import com.example.tasque.model.Task;
+import com.example.tasque.model.Project;
+import com.example.tasque.model.Tag;
+import com.example.tasque.model.TaskStatus;
+import com.example.tasque.model.User;
+import java.time.LocalDateTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TaskRepository extends JpaRepository<Task, String> {
+    List<Task> findByProject(Project project);
+    List<Task> findByAssignedTo(User assignee);
+    List<Task> findByProjectId(String projectId);
+    List<Task> findByTagsContaining(Tag tag);
+    List<Task> findByTagsAndProject(Tag tag, Project project);
+    List<Task> findByDeadlineBetweenAndStatusNot(LocalDateTime now, LocalDateTime nextDay, TaskStatus taskStatus);
+    boolean existsByProjectAndAssignedTo(Project project, User userToRemove);
 }
