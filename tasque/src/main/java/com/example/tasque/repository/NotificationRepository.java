2715093aa42c333913160a4b9ee4193f1a853038
athayanajwan <athaya123hafidz@gmail.com>
2025-05-31 07:29:15 +0700

    Update NotificationRepository.java

--- tasque/src/main/java/com/example/tasque/repository/NotificationRepository.java
+++ tasque/src/main/java/com/example/tasque/repository/NotificationRepository.java
@@ -2,19 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.example.tasque.repository;
+
+import com.example.tasque.model.Notification;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
  * @author regianyogaswara
  */
-package com.example.tasque.repository;
-
-import com.example.tasque.model.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findByRecipientId(String recipientId);
+public interface NotificationRepository extends JpaRepository<Notification, String> {
+    List<Notification> findByRecipientIdOrderByCreatedAtDesc(String recipientId);
+    long countByRecipientIdAndIsReadFalse(String recipientId);
 }
-
-
