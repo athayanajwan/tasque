05f16b9311cabc49b39d907e58da6f8091735249
athayanajwan <athaya123hafidz@gmail.com>
2025-05-31 07:32:52 +0700

    Update JwtUtil.java

--- tasque/src/main/java/com/example/tasque/security/JwtUtil.java
+++ tasque/src/main/java/com/example/tasque/security/JwtUtil.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Component
 public class JwtUtil {
@@ -36,13 +37,19 @@ public class JwtUtil {
                 .parseClaimsJws(token).getBody().getSubject();
     }
 
-    public boolean isTokenValid(String token) {
-        try {
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-            return true;
-        } catch (JwtException e) {
-            return false;
-        }
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+    try {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    } catch (JwtException e) {
+        return false;
+    }
+}
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parserBuilder().setSigningKey(key).build()
+            .parseClaimsJws(token).getBody().getExpiration();
+        return expiration.before(new Date());
     }
 }
 
