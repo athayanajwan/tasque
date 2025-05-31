588774d47f98669b6ceef8f0a11ee034d6f88d0a
athayanajwan <athaya123hafidz@gmail.com>
2025-05-31 07:37:01 +0700

    Update ProjectService.java

--- tasque/src/main/java/com/example/tasque/service/ProjectService.java
+++ tasque/src/main/java/com/example/tasque/service/ProjectService.java
@@ -3,193 +3,121 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.example.tasque.service;
-
+import com.example.tasque.dto.UserRequestDTO;
+import com.example.tasque.dto.UserResponseDTO;
+import com.example.tasque.model.User;
+import com.example.tasque.repository.UserRepository;
+import com.example.tasque.security.JwtUtil;
+import java.util.Objects;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 /**
  *
- * @author cachaww
+ * @author Athaya
  */
-import com.example.tasque.dto.ProjectRequestDTO;
-import com.example.tasque.dto.ProjectResponseDTO;
-import com.example.tasque.dto.ProjectResponseDTO.MemberInfo;
-import com.example.tasque.model.*;
-import com.example.tasque.repository.ProjectMembershipRepository;
-import com.example.tasque.repository.ProjectRepository;
-import com.example.tasque.repository.UserRepository;
-import com.example.tasque.service.ProjectService;
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.transaction.Transactional;
-import java.time.LocalDate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 @Service
-public class ProjectService {
+public class UserService {
     @Autowired
-    private ProjectRepository projectRepo;
-
+    private UserRepository userRepository;
+    
     @Autowired
-    private ProjectMembershipRepository membershipRepo;
-
+    private PasswordEncoder passwordEncoder;
+    
     @Autowired
-    private UserRepository userRepo;
+    private JwtUtil jwtUtil;
     
-    public ProjectResponseDTO createProject(ProjectRequestDTO request, User creator){
-        long count = projectRepo.countProject()+1;
-        String id = String.format("project-%05d", count);
-        Project project = new Project(id, request.getName(), request.getDescription(), LocalDate.now(), request.getDeadline(), creator);
-        
-        Project saved = projectRepo.save(project);
-        
-        String projectMemberId = creator.getId().substring(creator.getId().length() - 5)+"-"+id.substring(id.length() - 5);
-        
-        ProjectMembership membership = new ProjectMembership(projectMemberId, saved, creator, ProjectRole.PROJECT_MANAGER);
-         membershipRepo.save(membership);
-
-        return mapToResponseDTO(saved);
-    }
-    
-    public ProjectResponseDTO updateProject(String projectId, ProjectRequestDTO request, User requester) {
-        Project project = getProjectEntityById(projectId, requester);
-        requireManagerRole(project, requester);
-
-        project.setName(request.getName());
-        project.setDescription(request.getDescription());
-        project.setDeadline(request.getDeadline());
-
-        Project updated = projectRepo.save(project);
-        return mapToResponseDTO(updated);
-    }
-    
-    @Transactional
-    public void deleteProject(String projectId, User requester) {
-        Project project = getProjectEntityById(projectId, requester);
-        requireManagerRole(project, requester);
-        
-        List<ProjectMembership> memberships = membershipRepo.findByProject(project);
-        membershipRepo.deleteAll(memberships);
-        
-        projectRepo.delete(project);
-    }
-    
-    public List<ProjectResponseDTO> getProjectsByUser(User user) {
-        return membershipRepo.findByUser(user).stream()
-                .map(ProjectMembership::getProject)
-                .map(this::mapToResponseDTO)
-                .toList();
-    }
-    
-    public ProjectResponseDTO getProjectById(String projectId, User requester) {
-        Project project = getProjectEntityById(projectId, requester);
-        List<MemberInfo> memberInfos = membershipRepo.findByProject(project).stream()
-        .map(m -> {
-            MemberInfo info = new MemberInfo();
-            info.setUserId(m.getUser().getId());
-            info.setUsername(m.getUser().getUsername());
-            info.setRole(m.getRole());
-            return info;
-        }).collect(Collectors.toList());
-            
-        ProjectResponseDTO dto = new ProjectResponseDTO();
-        dto.setId(project.getId());
-        dto.setName(project.getName());
-        dto.setDescription(project.getDescription());
-        dto.setCreatedBy(project.getCreatedBy().getUsername());
-        dto.setCreatedAt(project.getCreatedAt());
-        dto.setDeadline(project.getDeadline());
-        dto.setMembers(memberInfos);
-
-        return dto;
-    }
-    
-    public void addMemberToProject(String projectId, String username, String roleStr, User requester) {
-        Project project = getProjectEntityById(projectId, requester);
-        requireManagerRole(project, requester);
-
-        User user = userRepo.findByUsername(username)
-                .orElseThrow(() -> new EntityNotFoundException("User tidak ditemukan"));
-
-        ProjectRole role = ProjectRole.valueOf(roleStr.toUpperCase());
-
-        if (membershipRepo.findByProjectAndUser(project, user).isPresent()) {
-            throw new IllegalStateException("User sudah menjadi anggota proyek ini");
+    public UserResponseDTO register(UserRequestDTO request){
+        if (userRepository.findByUsername(request.getUsername()).isPresent()){
+            throw new IllegalArgumentException("Username sudah digunakan");
         }
         
-        String projectMemberId = user.getId().substring(user.getId().length() - 5)+"-"+projectId.substring(projectId.length() - 5);
-        ProjectMembership member = new ProjectMembership();
-        member.setId(projectMemberId);
-        member.setProject(project);
-        member.setUser(user);
-        member.setRole(role);
-        membershipRepo.save(member);
-    }
-    
-    public void removeMemberFromProject(String projectId, String username, User requester) {
-    Project project = getProjectEntityById(projectId, requester);
-    requireManagerRole(project, requester);
-
-    User userToRemove = userRepo.findByUsername(username)
-            .orElseThrow(() -> new EntityNotFoundException("User tidak ditemukan"));
-
-    ProjectMembership membership = membershipRepo.findByProjectAndUser(project, userToRemove)
-            .orElseThrow(() -> new EntityNotFoundException("User bukan anggota proyek ini"));
-
-    if (userToRemove.getId().equals(requester.getId())) {
-        throw new IllegalStateException("Project Manager tidak dapat menghapus dirinya sendiri.");
-    }
-
-    membershipRepo.delete(membership);
-    }
-    
-
-    
-    private Project getProjectEntityById(String projectId, User requester) {
-        Project project = projectRepo.findById(projectId)
-                .orElseThrow(() -> new EntityNotFoundException("Project tidak ditemukan"));
-
-        membershipRepo.findByProjectAndUser(project, requester)
-                .orElseThrow(() -> new SecurityException("Anda bukan anggota proyek ini"));
-
-        return project;
-    }
-
-    private void requireManagerRole(Project project, User requester) {
-        ProjectMembership member = membershipRepo.findByProjectAndUser(project, requester)
-                .orElseThrow(() -> new SecurityException("Anda bukan anggota proyek ini"));
-
-        if (member.getRole() != ProjectRole.PROJECT_MANAGER) {
-            throw new SecurityException("Akses ditolak: hanya Project Manager yang dapat melakukan aksi ini");
+        if (userRepository.findByEmail(request.getEmail()).isPresent()){
+            throw new IllegalArgumentException("Email sudah digunakan");
         }
-    }
-    
-    private ProjectResponseDTO mapToResponseDTO(Project project) {
-        return new ProjectResponseDTO(
-                project.getId(),
-                project.getName(),
-                project.getDescription(),
-                project.getCreatedBy().getUsername(),
-                project.getCreatedAt(),
-                project.getDeadline()
-        );
-    }
-
-    public double getProjectProgress(String projectId) {
-        Optional<Project> projectOpt = projectRepo.findById(projectId);
-
-        if (projectOpt.isEmpty()) {
-            throw new IllegalArgumentException("Project with ID " + projectId + " not found.");
+        
+        long count = userRepository.countUsers()+1;
+        String id = String.format("user-%05d", count);
+        
+        User user = new User(id, request.getUsername(), request.getEmail(), passwordEncoder.encode(request.getPassword()));
+        
+        try {
+            userRepository.save(user);
+            System.out.println("User berhasil disimpan ke DB.");
+        } catch (Exception e) {
+            System.err.println("Gagal menyimpan user: " + e.getMessage());
+            e.printStackTrace();
         }
 
-        Project project = projectOpt.get();
-        return project.getProgress();
+        
+        return new UserResponseDTO(user.getId(), user.getUsername(), user.getEmail());
     }
     
-    public List<ProjectResponseDTO> getAllProjects() {
-        return projectRepo.findAll().stream().map(this::mapToResponseDTO).toList();
+    public String login(UserRequestDTO request){
+
+
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("Username/Password salah"));
+        
+        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())){
+            throw new BadCredentialsException("Username/Password salah");
+        }
+        
+        return jwtUtil.generateToken(user.getUsername());
     }
     
+    public UserResponseDTO updateProfile(String id, UserRequestDTO request){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException(""));
+        
+        if(!user.getUsername().equals(request.getUsername())){
+            userRepository.findByUsername(request.getUsername()).ifPresent(existing -> {throw new IllegalArgumentException("Username sudah digunakan");});
+            user.setUsername(request.getUsername());
+        }
+        
+        if(!user.getEmail().equals(request.getEmail())){
+            userRepository.findByEmail(request.getEmail()).ifPresent(existing -> {throw new IllegalArgumentException("Email sudah digunakan");});
+            user.setEmail(request.getEmail());
+        }
+        
+        if(request.getPassword() != null && !request.getPassword().isBlank()){
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+        }
+        
+        if (!Objects.equals(user.getPhoneNumber(), request.getPhoneNumber())) {
+            user.setPhoneNumber(request.getPhoneNumber());
+        }
+
+        if (!Objects.equals(user.getDeskripsi(), request.getDeskripsi())) {
+            user.setDeskripsi(request.getDeskripsi());
+        }
+        
+        userRepository.save(user);
+        
+        return new UserResponseDTO(user.getId(), user.getUsername(), user.getEmail(), user.getPhoneNumber(), user.getDeskripsi());
+    }
+    
+    public UserResponseDTO getCurrentUser(String username) {
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        
+        return new UserResponseDTO(user.getId(), user.getUsername(), user.getEmail(), user.getPhoneNumber(), user.getDeskripsi());
+    }
+    
+    public UserResponseDTO updateCurrentUser(String username, UserRequestDTO request) {
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+    return updateProfile(user.getId(), request);
+    }
+
+    public UserResponseDTO getUserByUsername(String username) {
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new RuntimeException("User tidak ditemukan"));
+    
+    
+    return new UserResponseDTO(user.getId(), user.getUsername(), user.getEmail(), user.getPhoneNumber(), user.getDeskripsi());
+}
 }
