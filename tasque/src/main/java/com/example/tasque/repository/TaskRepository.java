2cb600062385825ba6a92d29a90171f75cf20698
athayanajwan <athaya123hafidz@gmail.com>
2025-05-31 07:30:55 +0700

    Update TaskRepository.java

--- tasque/src/main/java/com/example/tasque/repository/TaskRepository.java
+++ tasque/src/main/java/com/example/tasque/repository/TaskRepository.java
@@ -1,7 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.example.tasque.repository;
-
-import com.example.tasque.model.Task;
+import com.example.tasque.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.Optional;
 
-public interface TaskRepository extends JpaRepository<Task, String> {
+public interface UserRepository extends JpaRepository<User, String>{
+    @Query("SELECT COUNT(u) FROM User u")
+    long countUsers();
+    
+    Optional<User> findByUsername(String username);
+    
+    Optional<User> findByEmail(String email);
+    
+    Optional<User> findById(String id);
 }
